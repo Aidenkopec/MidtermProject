d1f5613809181329c144ddf64071371009eb4931
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2022-06-28 16:23:00 -0600

    Adding links to compoenets

--- src/App.js
+++ src/App.js
@@ -13,8 +13,19 @@ function App() {
         <div>
             <Router>
                 <div className="App">
-
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About Us</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact Us</Link>
+                        </li>
+                    </ul>
                 </div>
+
             </Router>
         </div>
 
