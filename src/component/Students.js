3f25b2781b53666d0d607a342cf5b21c29f2d63f
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2022-06-28 17:07:07 -0600

    Adding Array list

---
+++ src/component/Students.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Students(props) {
+    const cardInfo = [
+        {
+            image:
+                "s1.png",
+            name: "Mike",
+            gpa: 3.8
+        },
+        {
+            image:
+                "s2.png",
+            name: "Manal",
+            gpa: 3.6
+        },
+        {
+            image:
+                "s3.png",
+            name: "Moe",
+            gpa: 3.2
+        },
+        {
+            image:
+                "s4.png",
+            name: "Mary",
+            gpa: 2.6
+        }
+    ];
+    // const studentList = cardInfo.map( students=> <Person key={students.name} person = {students}/>)
+    // return <div>{studentList}</div>
+    return
+
+}
+
+export default Students;
