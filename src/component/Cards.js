0c9630156280f231dbbcf5186b8b129779f24ddf
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2022-06-29 12:45:27 -0600

    Corrected card layout

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import {MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn} from 'mdb-react-ui-kit';
+import {Button} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
+import '../cardStyle.css';
 
 function Cards({student}) {
     return (
-        <div>
-            <MDBCard style={{maxWidth: '6rem'}}>
-                <MDBCardImage src={student.image} position='top' alt='...'/>
-                <MDBCardBody>
-                    {/*<MDBCardTitle>Card title</MDBCardTitle>*/}
-                    <MDBCardText >
-                        <h1>{student.name}</h1>
-                        <h3>{student.gpa}</h3>
-                    </MDBCardText>
-                    <MDBBtn href='#'>Button</MDBBtn>
-                </MDBCardBody>
-                <br/>
-                <br/>
-                <br/>
-            </MDBCard>
+
+        <div className='card-container'>
+            <Card style={{width: '15rem'}}>
+                <Card.Img class="top" src={student.image}/>
+                <Card.Body class="card-body">
+                    <Card.Title>Name: {student.name}</Card.Title>
+                    <Card.Text>
+                        GPA: {student.gpa}
+                    </Card.Text>
+                    <Button variant="primary">Grade</Button>
+                </Card.Body>
+            </Card>
         </div>
+
     );
 }
 
